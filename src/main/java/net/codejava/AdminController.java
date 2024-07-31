5582f011eacdcb595dd52dc77b941a6723198c82
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-07-31 10:59:37 +0530

    Update AdminController.java

--- src/main/java/net/codejava/AdminController.java
+++ src/main/java/net/codejava/AdminController.java
@@ -2,7 +2,7 @@ package net.codejava;
 
 
 
-import org.springframework.stereotype.Controller;  
+import org.springframework.stereotype.Controller;   
 import org.springframework.web.bind.annotation.GetMapping;      
 import org.springframework.web.bind.annotation.PostMapping;     
 import org.springframework.web.bind.annotation.RequestParam;
