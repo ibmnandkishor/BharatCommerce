2db7d7ab214b9f7ff0475e45f9e605b1e9ce246b
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-07-31 11:21:28 +0530

    Update UserRepositoryTests.java

--- src/test/java/net/codejava/UserRepositoryTests.java
+++ src/test/java/net/codejava/UserRepositoryTests.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest; 
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;  
- 
+  
 @DataJpaTest    
 @AutoConfigureTestDatabase(replace = Replace.NONE)    
 @Rollback(false)          
