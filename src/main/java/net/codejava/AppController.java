f410e69e2646f6100a21e9e803a7608481d1a96b
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-08-01 09:26:09 +0530

    Update AppController.java

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;   
 import org.springframework.web.bind.annotation.GetMapping;   
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping; 
 
 @Controller
 public class AppController {
