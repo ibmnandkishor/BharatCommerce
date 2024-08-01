29f9e2931111e9d612da1c32351c7b0480359605
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-08-01 09:26:33 +0530

    Update CustomUserDetails.java

--- src/main/java/net/codejava/CustomUserDetails.java
+++ src/main/java/net/codejava/CustomUserDetails.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class CustomUserDetails implements UserDetails {
 
-	private User user;
+	private User user; 
 	  
 	public CustomUserDetails(User user) {
 		this.user = user;
