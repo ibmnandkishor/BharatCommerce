292d6950f00bbfd5ecaf9dea93d9cf23f5265f6c
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-08-01 15:56:24 +0530

    Delete src/main/repository directory

--- src/main/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package net.codejava.repository;
-
-import net.codejava.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
