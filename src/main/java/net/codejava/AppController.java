5ad7825d307b25c82526944080b425470b4e6c5b
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-08-06 16:54:04 +0530

    Update AppController.java

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
                   
 @Controller  
-public class AppController {
+public class AppController { 
 
     @Autowired
     private UserRepository userRepo;
