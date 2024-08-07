70d6a82a764e6526b9201f906bbc8ae1832a4887
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-08-07 16:24:02 +0530

    Update AppController.java

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -2,7 +2,7 @@ package net.codejava;
 
 import java.util.List;
  
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
