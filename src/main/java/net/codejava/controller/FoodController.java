113e8ad865bacc9f14d8b855cbe47fdf20151f59
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-08-08 16:57:23 +0530

    Update FoodController.java

--- src/main/java/net/codejava/controller/FoodController.java
+++ src/main/java/net/codejava/controller/FoodController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller 
 @RequestMapping("/food")
 public class FoodController {
 	
